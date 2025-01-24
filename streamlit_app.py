52c71da9bc36ec1b0f54544ffa2b89530c7cb281
keerthana <144551341+keerthi7612@users.noreply.github.com>
2025-01-24 12:03:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 st.title("Pregnancy prediction app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+title = st.text_input("PREDICTION", "Enter your Age")
