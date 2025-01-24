e6683610215c2e857258f1f89c77ddaeb6a1e766
keerthana <144551341+keerthi7612@users.noreply.github.com>
2025-01-24 12:06:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title("Pregnancy prediction app")
 title = st.text_input("Enter your Age")
+title = st.text_input("Enter your IBM")
+title = st.text_input("Enter your Pressure")
+title = st.text_input("Enter your BP")
+title = st.text_input("Enter your Sugar")
